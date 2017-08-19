b23feb181692878a8e5cdaacd14c5f25f16fe064
Riley Shea <rileymshea@gmail.com>
2017-08-18 22:22:23 -0400

    Initial commit

---
+++ src/com/datastructures/Main.java
@@ -0,0 +1,13 @@
+package com.datastructures;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int x = 1;
+        if (x == 1){
+            return;
+        }
+    }
+}
